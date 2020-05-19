b9d7d41090d08a3e2a757a578eeff9199770f216
gdmyayaly <devyayaly@gmail.com>
2020-05-19 13:11:02 +0000

    deploy1

--- src/app/pages/quizz/quizz.component.ts
+++ src/app/pages/quizz/quizz.component.ts
@@ -6,10 +6,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quizz.component.css']
 })
 export class QuizzComponent implements OnInit {
-
+  public isgame:boolean=true;
+  public data=[
+    {id:1,titre:'C\'est quoi Internet ?',image1:'assets/image/q1c.png',image2:'assets/image/q1b.png',image3:'assets/image/q1a.png',good:'Un réseau mondial permettant de connecter tous les ordinateurs entre',titre1:'Un réseau téléphonique permettant de relier les personnes',titre2:'Un réseau mondial permettant de connecter tous les ordinateurs entre',titre3:'un réseau mobile',etat:'',youresponse:''},
+    {id:2,titre:'C\'est quoi un navigateur web ?',image1:'assets/image/q2a.png',image2:'assets/image/q1g.png',image3:'assets/image/q2b.png',good:'Un logiciel qui permet d\'aller sur Internet',titre1:'Un périphérique pour Internet',titre2:'Un logiciel qui permet d\'aller sur Internet',titre3:'Une personne qui nous assiste sur Internet',etat:'',youresponse:''},
+  ]
+  public question={id:1,titre:'C\'est quoi Internet ?',image1:'assets/image/q1c.png',image2:'assets/image/q1b.png',image3:'assets/image/q1a.png',good:'Un réseau mondial permettant de connecter tous les ordinateurs entre',titre1:'Un réseau téléphonique permettant de relier les personnes',titre2:'Un réseau mondial permettant de connecter tous les ordinateurs entre',titre3:'un réseau mobile',etat:'',youresponse:''};
+  public tab=1;
+  public compte=0;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
-
+rep(titre){
+  console.log(titre);
+  
+  this.data[this.tab-1].etat='faux';
+  this.data[this.tab-1].youresponse=titre;
+  if (this.question.good==titre) {
+    this.compte++;
+    this.data[this.tab-1].etat='vrai';
+  }
+  if (this.tab<2) {
+  this.question=this.data[this.tab];
+  this.tab++
+  }
+  else{
+    this.isgame=false;
+    console.log(this.data);
+  }
+}
 }
